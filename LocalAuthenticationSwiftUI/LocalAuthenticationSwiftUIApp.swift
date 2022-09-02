69e4bb51714bc9daf971848af42a0265a1a08c30
Fhikar Zul <Fhikarzul18@gmail.com>
2022-09-02 11:33:03 +0800

    Initial Commit

---
+++ LocalAuthenticationSwiftUI/LocalAuthenticationSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalAuthenticationSwiftUIApp.swift
+//  LocalAuthenticationSwiftUI
+//
+//  Created by Lucy on 02/09/22.
+//
+
+import SwiftUI
+
+@main
+struct LocalAuthenticationSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
